8484e6566eb74fe12d945302df68370c5a3af1dd
TaehoLEEKR <679748@naver.com>
2023-09-06 13:33:02 +0900

    first commit

---
+++ src/main/java/com/example/groomthon_camping/GroomthonCampingApplication.java
@@ -0,0 +1,13 @@
+package com.example.groomthon_camping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomthonCampingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroomthonCampingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/groomthon_camping/GroomthonCampingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groomthon_camping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomthonCampingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
